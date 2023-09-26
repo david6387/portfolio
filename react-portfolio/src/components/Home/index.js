5f08198b7345d78eaa942b1d17272a4da14e0d13
david <cognet.david@hotmail.fr>
2023-09-26 13:33:15 +0200

    start page "home"

---
+++ react-portfolio/src/components/Home/index.js
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-d.png'
+import './index.scss'
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Bonjour, <br /> Je m'appelle
+          <img src={LogoTitle} alt="developer" />
+          avid
+          <br />
+          développeur web
+        </h1>
+        <h2>
+          Développeur sur la stack MERN / MongoDB / Express / React / Node
+        </h2>
+        <Link to="/contact" className="flat-button">
+          ECRIVEZ-MOI
+        </Link>
+      </div>
+    </div>
+  )
+}
+export default Home
