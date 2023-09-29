d0f9a1788cc93471a157ca4f712fa23663318e7a
david <cognet.david@hotmail.fr>
2023-09-29 13:11:59 +0200

    add map

--- react-portfolio/src/components/Contact/index.js
+++ react-portfolio/src/components/Contact/index.js
@@ -3,6 +3,7 @@ import AnimatedLetters from '../AnimatedLetters/index'
 import Loader from 'react-loaders'
 import { useEffect, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Contact = () => {
   // const serviceId = process.env.REACT_APP_SERVICE_ID
@@ -107,6 +108,23 @@ const Contact = () => {
               </ul>
             </form>
           </div>
+        </div>
+        <div className="info-map">
+          David,
+          <br />
+          France
+          <br />
+          Bagneux, 92220
+          <br />
+          <span>davidcognetm@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[48.795294, 2.314432]} zoom={12}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[48.795294, 2.314432]}>
+              <Popup>David habite par ici</Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
       <Loader type="cube-transition" />
