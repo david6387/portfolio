8e3425eddc7258bcd6e08fc581719a7f3f8fdba6
david <cognet.david@hotmail.fr>
2023-10-03 18:28:56 +0200

    add skills & replace description portfolio

--- react-portfolio/src/components/Portfolio/index.js
+++ react-portfolio/src/components/Portfolio/index.js
@@ -28,7 +28,8 @@ const Portfolio = () => {
                 />
                 <div className="content">
                   <p className="title">{port.title}</p>
-                  <h4 className="description">{port.description}</h4>
+                  <p className="description">{port.description}</p>
+                  <p className="skills">{port.skills}</p>
                   <button className="btn" onClick={() => window.open(port.url)}>
                     Découvrir
                   </button>
